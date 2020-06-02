9faf46af51437175f8de5e12afbab70e17f09829
Adam Whittingham <ad@cronofy.com>
2020-06-02 09:39:42 +0100

    Update dependencies
    Key updates are:
    Rspec 2 -> 3
    Hatchet 0.2.7 -> 0.2.13

--- spec/airbrake_appender_spec.rb
+++ spec/airbrake_appender_spec.rb
@@ -9,27 +9,28 @@ describe "AirbrakeAppender" do
       @error = StandardError.new
       @backtrace = [random_string, random_string]
       @error.stub(:backtrace) { @backtrace }
+      @error_message = random_string
 
-      @message = Hatchet::Message.new(@error_message = random_string, @error)
+      @message = Hatchet::Message.new(message: @error_message, error: @error)
     end
 
-    subject { @appender.add(@level, @context, @message) }
+    let(:message) { Hatchet::Message.new(message: @error_message, error: @error) }
+    subject { @appender.add(@level, @context, message) }
 
-    it "should notify airbrake" do
+    it "notifies airbrake" do
       expected_options = {
         :error_message => @error_message,
         :backtrace => @backtrace
       }
-      ::Airbrake.should_receive(:notify).with(@error, expected_options)
+      expect(::Airbrake).to receive(:notify).with(@error, expected_options)
       subject
     end
 
     context "when error is not present" do
-      before(:each) do
-        @message = Hatchet::Message.new(random_string)
-      end
-      it "should not notify airbrake" do
-        ::Airbrake.should_not_receive(:notify)
+      let(:message) { Hatchet::Message.new(random_string) }
+
+      it "does not notify airbrake" do
+        expect(::Airbrake).to_not receive(:notify)
         subject
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 require_relative '../lib/hatchet_airbrake'
 require 'rspec'
 
-RSpec.configure do |config|
-  config.color_enabled = true
-end
-
 def random_string
   (0...24).map{ ('a'..'z').to_a[rand(26)] }.join
 end
